844981693e27a66c51fca347992b77c7313331d4
Gretel Rodriguez <gretelrodriguez42@gmail.com>
2022-09-20 19:13:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let definition = document.getElementById("definition")
+let input = document.querySelector(".input")
+
+
+
+let renderWord = (aWord) => {
+    return new Promise((resolve, reject) =>{
+        let req = new XMLHttpRequest();
+        req.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${aWord}`);
+        req.addEventListener('readystatechange', ()=> {
+            if(req.status === 200){
+                try{
+                    let word = JSON.parse(req.response)[0]
+    
+                    resolve(word)
+                }
+                catch(err){
+                    console.log()
+                }
+                
+            }
+            else(
+                reject("Not a word")
+            )
+            
+        })
+        req.send();
+    })
+}
+
+
+input.addEventListener("input",()=>{
+   
+    renderWord(input.value).then(word =>{
+        definition.innerText = `${word.word} : `
+        word.meanings.forEach(meaning => {
+             meaning.definitions.forEach(definitions =>{
+                definition.innerHTML += `<br> ${definitions.definition}`
+             })
+        })
+}).catch(ntword => {
+        definition.textContent = ntword
+    })
+})
+
+
